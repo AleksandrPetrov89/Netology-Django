a673744fb1ebe34a669948c11c268e3f66c27c8a
AleksandrPetrov89 <petrovav89@gmail.com>
2022-08-27 17:37:46 +0500

    Task 2.1 completed

--- 2.1-databases/models_list_displaying/books/converters.py
+++ 2.1-databases/models_list_displaying/books/converters.py
@@ -1,13 +1,13 @@
-from datetime import datetime
+from datetime import datetime, date
 
 
 class PubDateConverter:
     regex = r'[0-9]{4}-[0-9]{2}-[0-9]{2}'
     format = '%Y-%m-%d'
 
-    def to_python(self, value: str) -> datetime:
-        return datetime.strptime(value, self.format)
+    def to_python(self, value: str) -> date:
+        return datetime.strptime(value, self.format).date()
 
-    def to_url(self, value: datetime) -> str:
+    def to_url(self, value: date) -> str:
         return value.strftime(self.format)
 
--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date
 
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
@@ -10,11 +10,14 @@ def index(request):
     return redirect('books')
 
 
+books = Book.objects.all().order_by("pub_date")
+all_date = [book.pub_date for book in books]
+
+
 def books_view(request):
     template = 'books/books_list.html'
     page_number = int(request.GET.get("page", 1))
-    books = Book.objects.all()
-    paginator = Paginator(books, 9)
+    paginator = Paginator(books, 2)
     page = paginator.get_page(page_number)
     context = {
         'page': page,
@@ -22,13 +25,25 @@ def books_view(request):
     return render(request, template, context)
 
 
-def view_books_pub_date(request, pub_date: datetime):
+def view_books_pub_date(request, pub_date: date):
+    if pub_date not in all_date:
+        return redirect('books')
     template = 'books/books_pub_date.html'
     page_number = int(request.GET.get("page", 1))
-    books = Book.objects.all()
-    paginator = Paginator(books, 9)
+    books_pub_date = books.filter(pub_date=pub_date)
+    date_before, date_after = None, None
+    for book in books:
+        if book.pub_date < pub_date:
+            date_before = book.pub_date
+        if book.pub_date > pub_date:
+            date_after = book.pub_date
+            break
+    paginator = Paginator(books_pub_date, 2)
     page = paginator.get_page(page_number)
     context = {
         'page': page,
+        'date_before': date_before,
+        'date_after': date_after
+
     }
     return render(request, template, context)
