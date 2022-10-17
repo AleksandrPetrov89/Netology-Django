a22f642dde1244f0d85b3a9938c0dbd65feeb86e
AleksandrPetrov89 <petrovav89@gmail.com>
2022-10-17 11:13:24 +0500

    Task 2.2 completed

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -8,21 +8,27 @@ from .models import Article, Tag, Relationship
 
 class RelationshipInlineFormset(BaseInlineFormSet):
     def clean(self):
+        is_main_counter = 0
         for form in self.forms:
             # В form.cleaned_data будет словарь с данными
             # каждой отдельной формы, которые вы можете проверить
-            print(form.cleaned_data)
+            if 'is_main' in form.cleaned_data:
+                if form.cleaned_data['is_main']:
+                    is_main_counter += 1
             # вызовом исключения ValidationError можно указать админке о наличие ошибки
             # таким образом объект не будет сохранен,
             # а пользователю выведется соответствующее сообщение об ошибке
-            # raise ValidationError('Тут всегда ошибка')
+        if is_main_counter == 0:
+            raise ValidationError('Укажите основной раздел!')
+        elif is_main_counter > 1:
+            raise ValidationError('Основным может быть только один раздел!')
         return super().clean()  # вызываем базовый код переопределяемого метода
 
 
 class RelationshipInline(admin.TabularInline):
     model = Relationship
     extra = 1
-    # formset = RelationshipInlineFormset
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
@@ -32,4 +38,4 @@ class ArticleAdmin(admin.ModelAdmin):
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
-    inlines = [RelationshipInline]
+    pass
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -10,7 +10,7 @@ def articles_list(request):
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
 
-    object_list = Article.objects.all().order_by(ordering)
+    object_list = Article.objects.all().order_by(ordering).prefetch_related('scopes')
 
     context = {'object_list': object_list}
 
