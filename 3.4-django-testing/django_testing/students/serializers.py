302a6ea5e6bad397076ed018db3590a416cdf2ab
AleksandrPetrov89 <petrovav89@gmail.com>
2022-11-13 18:56:47 +0500

    Almost did the extra task

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from django_testing.settings import MAX_STUDENTS_PER_COURSE
 from students.models import Course
 
 
@@ -8,3 +9,17 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def validate(self, data):
+        """Метод для валидации. Вызывается при создании и обновлении."""
+        quantity_students = 0
+        course = self._args
+        if 'students' in data.keys():
+            if len(course) == 0:
+                quantity_students = len(data['students'])
+            else:
+                q_s = len(list(course[0].students.all()))
+                quantity_students = len(data['students']) + q_s
+        if quantity_students > MAX_STUDENTS_PER_COURSE:
+            raise serializers.ValidationError('Превышено максимальное число студентов на курсе!')
+        return data
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,4 +1,5 @@
 import pytest
+import pytest_django
 import random
 from model_bakery import baker
 from rest_framework.test import APIClient
@@ -155,3 +156,67 @@ def test_course_deletion(client, course_factory):
     # Assert
     assert response.status_code == 204
     assert Course.objects.count() == count - 1
+
+
+@pytest.mark.parametrize(
+    ['quantity', 'status_code'],
+    (
+        (3, 201),
+        (20, 201),
+        (21, 400)
+    )
+)
+@pytest.mark.django_db
+def test_course_create_validation(client, student_factory, course_factory,
+                                  settings, quantity, status_code):
+
+    # Arrange
+    settings.MAX_STUDENTS_PER_COURSE = 2
+
+    students = student_factory(_quantity=quantity)
+    students_id = [student.id for student in students]
+    url = f'/api/v1/courses/'
+    data = {'name': 'Курс № 3', 'students': students_id}
+
+    # Act
+    response = client.post(url, data=data, format='json')  # Формат можно задать в settings.py
+
+    # Assert
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    ['quantity_1', 'status_code_1', 'quantity_2', 'status_code_2'],
+    (
+        (3, 200, 2, 200),
+        (10, 200, 10, 200),
+        (21, 400, 5, 200),
+        (5, 200, 16, 400),
+        (21, 400, 22, 400)
+    )
+)
+@pytest.mark.django_db
+def test_course_update_validation(client, course_factory, student_factory, settings,
+                                  quantity_1, status_code_1, quantity_2, status_code_2):
+
+    # Arrange
+    settings.MAX_STUDENTS_PER_COURSE = 2
+
+    students_1 = student_factory(_quantity=quantity_1)
+    students_1_id = [student.id for student in students_1]
+    data_1 = {'name': 'Курс № 4a', 'students': students_1_id}
+
+    students_2 = student_factory(_quantity=quantity_2)
+    students_2_id = [student.id for student in students_2]
+    data_2 = {'name': 'Курс № 4b', 'students': students_2_id}
+
+    course = course_factory(_quantity=1)[0]
+    url = f'/api/v1/courses/{course.id}/'
+
+    # Act
+    response_1 = client.patch(url, data=data_1, format='json')  # Формат можно задать в settings.py
+    response_2 = client.patch(url, data=data_2, format='json')  # Формат можно задать в settings.py
+
+    # Assert
+    assert response_1.status_code == status_code_1
+    assert response_2.status_code == status_code_2
